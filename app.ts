d94ce51d186b1a516af6944cea59a39b3262a048
Martin Tracey <matracey@outlook.ie>
2016-11-07 20:41:46 +0000

    Interfaces

--- app.ts
+++ app.ts
@@ -1,15 +1,15 @@
-function totalLength(x: string, y: string): number
-function totalLength(x: any[], y: any[]): number
-function totalLength(x: (string | any[]), y: (string | any[])): number {
-    var total: number = x.length + y.length;
+interface Todo {
+    name: string;
+    completed?: boolean; // ? represents an optional property.
+}
 
-    if (x instanceof Array) {
-        x.push('abc');
-    };
+interface ITodoService {
+    add(todo: Todo): Todo;
+    delete(todoId: number): void;
+    getAll(): Todo[];
+    getById(todoId: number): Todo;
+}
 
-    if (x instanceof String) {
-        x.substr(1);
-    };
-
-    return total;
+var todo: Todo = {
+    name: "Pick up drycleaning"
 }

05503bb61bf0970be8cb4a1c42c8066f7570ebc1
Martin Tracey <matracey@outlook.ie>
2016-11-07 20:54:05 +0000

    Interface extensions

--- app.ts
+++ app.ts
@@ -1,11 +1,43 @@
-interface jQuery {
-    (selector: string): HTMLElement;
-    version: number;
-}
-
-var $ = <jQuery>function(selector) {
+var $ = <jQuery>function(selector: string) {
     // find DOM element
 }
 
-$.version = 1.12;
-var element = $('#container');
+$.version = 1.18;
+
+interface Todo {
+    name: string;
+    completed?: boolean;
+}
+
+interface jQuery {
+    (selector: (string | any)): jQueryElement;
+    fn: any;
+    version: number;
+}
+
+interface jQueryElement {
+    data(name: string): any;
+    data(name: string, data: any): jQueryElement;
+}
+
+interface jQueryElement {
+    todo(): Todo;
+    todo(todo: Todo): jQueryElement;
+}
+
+$.fn.todo = function(todo?: Todo): Todo {
+    if (todo) {
+        $(this).data('todo', todo);
+    } else {
+        return $(this).data('todo');
+    }
+}
+
+var todo = { name: "Pick up drycleaning" };
+var container = $('container');
+
+//container.data('todo', todo);
+//var savedTodo = container.data('todo');
+
+container.todo(todo);
+var savedTodo = container.todo();

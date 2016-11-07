588e819cfb5c76fdd446be7b55bf40c9984b1a89
Martin Tracey <matracey@outlook.ie>
2016-11-07 19:50:17 +0000

    Function overloads.

--- app.ts
+++ app.ts
@@ -1,3 +1,5 @@
+function totalLength(x: string, y: string): number
+function totalLength(x: any[], y: any[]): number
 function totalLength(x: (string | any[]), y: (string | any[])): number {
     var total: number = x.length + y.length;
 
